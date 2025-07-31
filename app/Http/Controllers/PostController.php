83cce942a71d6a49de216c83654b002f9cc78df9
M.Mahdi Saeidi <M.Mahdisaeidi99@gmail.com>
2025-07-31 13:39:02 +0330

    feat: add post view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::latest()->simplePaginate(5);
-        return view('dashboard', compact('posts'));
+        return view('post.index', compact('posts'));
     }
 
     /**
