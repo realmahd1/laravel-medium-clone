fe10853353144a437a8097c770ab5a87c3295daf
M.Mahdi Saeidi <M.Mahdisaeidi99@gmail.com>
2025-08-02 11:41:35 +0330

    feat(profile): add `image`,`username` and `bio` fields

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,6 +17,22 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
+            'username' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique(User::class)->ignore($this->user()->id)
+            ],
+            'image' => [
+                'nullable',
+                'image',
+                'mimes:jpge,png,jpg,gif,svg',
+                'max:2048'
+            ],
+            'bio' => [
+                'nullable',
+                'string',
+            ],
             'email' => [
                 'required',
                 'string',
